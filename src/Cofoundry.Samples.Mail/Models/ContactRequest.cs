da7654b43b1ed141015a65753c4b0e8a24ae6f29
Joel Mitchell <2780344+HeyJoel@users.noreply.github.com>
2022-03-11 15:44:42 +0000

    Initial commit

---
+++ src/Cofoundry.Samples.Mail/Models/ContactRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cofoundry.Samples.Mail
+{
+    /// <summary>
+    /// A simple contact request model used in our
+    /// contact form.
+    /// </summary>
+    public class ContactRequest
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
